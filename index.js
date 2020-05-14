ade44dce04496c1146c1245afc86d30f47a22856
Vercillius <vercillius.mila@smarterrobotics.io>
2020-05-14 13:40:39 +0000

    static files

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express');
 
 const app = express();
 
-const port = 3001;
 
+const port = 3001;
+app.use(express.static('static'));
 app.get('/',(req,res) => res.send('Welcome to RXR. Website coming soon'));
 
 app.listen(port,()=> console.log(`Running on port: ${port}`));
