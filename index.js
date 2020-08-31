432ee09585ec61d30d96e8f733c98abc435fc9dc
vercillius <vercillius.mila@smarterrobotics.io>
2020-08-31 02:40:22 +0000

    working

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var path = require('path');
 const app = express();
 
 
-const port = 3001;
+const port = process.env.PORT || 8080;
 app.use(express.static('static'));
 app.get('/press',(req,res) => res.sendFile(path.join(__dirname + '/static/press.html')));
 app.get('/',(req,res) => res.sendFile(path.join(__dirname + '/static/index.html')));
